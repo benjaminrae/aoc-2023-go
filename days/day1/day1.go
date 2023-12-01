8dcb0e38f83e5410b1cc74c3b41a3cbf1034e40d
Benjamin Rae <benjaminrae93@gmail.com>
2023-12-01 15:46:26 +0100

    feat: combine ParseLine output

--- days/day1/day1.go
+++ days/day1/day1.go
@@ -21,14 +21,30 @@ func ParseLine(line string) []int {
 	return numbers
 }
 
+func CombineFirstAndLast(numbers []int) (combined int, err error) {
+	var first = numbers[0]
+	var last = numbers[len(numbers)-1]
+
+	var joined = fmt.Sprintf("%v%v", first, last)
+
+	num, err := strconv.Atoi(joined)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return num, nil
+}
+
 func Solve() {
 	var line1 = "1abc2"
 	var line2 = "pqr3stu8vwx"
 	var line3 = "a1b2c3d4e5f"
 	var line4 = "treb7uchet"
 
-	fmt.Println(ParseLine(line1))
-	fmt.Println(ParseLine(line2))
-	fmt.Println(ParseLine(line3))
-	fmt.Println(ParseLine(line4))
+	fmt.Println(CombineFirstAndLast(ParseLine(line1)))
+	fmt.Println(CombineFirstAndLast(ParseLine(line2)))
+	fmt.Println(CombineFirstAndLast(ParseLine(line3)))
+	fmt.Println(CombineFirstAndLast(ParseLine(line4)))
+
 }
--- days/day1/day1_test.go
+++ days/day1/day1_test.go
@@ -17,9 +17,29 @@ var parseLineTestCases = []parseLineTestCase{
 	{"treb7uchet", []int{7}},
 }
 
+type combineFirstAndLastTestCase struct {
+	arg1     []int
+	expected int
+}
+
+var combineFirstAndLastCases = []combineFirstAndLastTestCase{
+	{[]int{1, 2}, 12},
+	{[]int{3, 8}, 38},
+	{[]int{1, 2, 3, 4, 5}, 15},
+	{[]int{7}, 77},
+}
+
 func TestParseLine(t *testing.T) {
 	for _, test := range parseLineTestCases {
 		if output := ParseLine(test.arg1); !reflect.DeepEqual(output, test.expected) {
+			t.Errorf("Output %q not equal to expected %q", output, test.expected)
+		}
+	}
+}
+
+func TestCombineFirstAndLast(t *testing.T) {
+	for _, test := range combineFirstAndLastCases {
+		if output, _ := CombineFirstAndLast(test.arg1); output != test.expected {
 			t.Errorf("Output %q not equal to expected %q", output, test.expected)
 		}
 	}
